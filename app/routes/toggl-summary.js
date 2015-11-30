7d3d3b81a154898fb439179269f9f61e2375c9f0
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-11-30 19:50:13 +0100

    add toggl-summary-error

--- app/routes/toggl-summary.js
+++ app/routes/toggl-summary.js
@@ -2,13 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    let queryParams = { user_agent: 'martinciu', workspace_id: ***REMOVED***, since: '2015-11-01', user_ids: ***REMOVED*** };
-    return this.store.queryRecord('toggl-summary', queryParams);
-  },
-
-  actions: {
-    error: function(reason) {
-      this.transitionTo("settings");
-    }
+    return this.store.queryRecord('toggl-summary', { user_agent: 'martinciu', workspace_id: ***REMOVED***, since: '2015-11-01', user_ids: ***REMOVED*** });
   }
 });
