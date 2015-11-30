c0c6c27a53edb60a3a7cd2cb03ee74fa9947c0e0
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-11-30 23:27:53 +0100

    add toggl-user

--- app/adapters/toggl-summary.js
+++ app/adapters/toggl-summary.js
@@ -2,10 +2,16 @@ export default DS.JSONAPIAdapter.extend({
   host: 'https://toggl.com',
   namespace: 'reports/api/v2',
   headers: function() {
-    if(this.session.isAuthorized()) {
-      return { "Authorization": "Basic " + this.get('session.settings.togglApiKey') };
-    }
+    return { "Authorization": "Basic " + this.get('session.settings.togglApiKey') };
   }.property("session.settings.togglApiKey"),
+
+  urlForQueryRecord: function(query, modelName) {
+    query["user_agent"] = "fajrant"
+    query["workspace_id"] = this.session.get("settings.workspaceId");
+    query["user_id"] = this.session.get("settings.userId");
+
+    return this._super(query, modelName);
+  },
 
   pathForType: function(type) {
     return 'summary';
---
+++ app/adapters/toggl-user.js
@@ -0,0 +1,12 @@
+export default DS.JSONAPIAdapter.extend({
+  host: 'https://toggl.com',
+  namespace: 'api/v8',
+  headers: function() {
+    return { "Authorization": "Basic " + this.get('session.settings.togglApiKey') };
+  }.property("session.settings.togglApiKey"),
+
+  pathForType: function(type) {
+    return 'me';
+  }
+
+});
