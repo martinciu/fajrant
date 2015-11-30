8ff3b7462a2c3b93548b249c3fbead9eb4157bef
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-11-30 19:45:32 +0100

    add settings

--- app/adapters/toggl-summary.js
+++ app/adapters/toggl-summary.js
@@ -1,9 +1,11 @@
 export default DS.JSONAPIAdapter.extend({
   host: 'https://toggl.com',
   namespace: 'reports/api/v2',
-  headers: {
-    'Authorization': 'Basic Y2FiOGFlNTE4MDg1ZjA4YmIwZmE5OTE1ODJhOTU2NjA6YXBpX3Rva2Vu'
-  },
+  headers: function() {
+    if(this.session.isAuthorized()) {
+      return { "Authorization": "Basic " + this.get('session.settings.togglApiKey') };
+    }
+  }.property("session.settings.togglApiKey"),
 
   pathForType: function(type) {
     return 'summary';
