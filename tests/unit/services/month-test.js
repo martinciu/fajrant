95c922b8bd05932ae78393bfc8ecfe351d731c76
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-12-02 13:10:57 +0100

    add month specs

--- tests/unit/services/month-test.js
+++ tests/unit/services/month-test.js
@@ -30,4 +30,23 @@ test('it knows end of the month', function(assert) {
   assert.equal(service.get('endsOn').format(), "2015-11-30T23:59:59+01:00");
 });
 
+test('it counts work days done', function(assert) {
+  let service = this.subject({ currentDate: moment('2015-11-20') });
+
+  assert.equal(service.get("workDaysDone"), 15);
+});
+
+test('it counts work days total', function(assert) {
+  let service = this.subject({ currentDate: moment('2015-11-20') });
+
+  assert.equal(service.get("workDaysTotal"), 20);
+});
+
+test('it counts work days left', function(assert) {
+  let service = this.subject({ currentDate: moment('2015-11-20') });
+
+  assert.equal(service.get("workDaysLeft"), 6);
+});
+
+
 // test('j  ')
