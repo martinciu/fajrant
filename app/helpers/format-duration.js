8914c95d6f9f386aa2d520e8be6584a42223aa86
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-12-02 21:32:37 +0100

    fix format-duration helper

--- app/helpers/format-duration.js
+++ app/helpers/format-duration.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import moment from 'moment';
 
-export function formatDuration(duration) {
+export function formatDuration(params) {
+  let duration = params[0];
   let hours = Math.round(duration.asHours());
   let minutes = duration.minutes();
   let minutesString = minutes < 10 ? `0${minutes}`:`${minutes}`;
